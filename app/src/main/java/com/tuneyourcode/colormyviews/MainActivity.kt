ffbb7a25488be1f2f383c73f47cbf5b75952f284
VARUN KUMAR <varunjha089@gmail.com>
2021-06-17 11:28:57 +0530

    working with baseline in android studio added buttons to interact with it.

--- app/src/main/java/com/tuneyourcode/colormyviews/MainActivity.kt
+++ app/src/main/java/com/tuneyourcode/colormyviews/MainActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+// comes from 'kotlin-android-extensions' plugin
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +18,11 @@ class MainActivity : AppCompatActivity() {
 //    val view1 : View = findViewById(R.id)
 
     private fun setListeners(){
+        // import kotlinx.android.synthetic.main.activity_main.*
         val clickableViews: List<View> =
-            listOf(findViewById(R.id.box_one_text), findViewById(R.id.box_two_text),
-                findViewById(R.id.box_three_text), findViewById(R.id.box_four_text),
-                findViewById(R.id.box_five_text), findViewById(R.id.constraint_layout))
+            listOf(box_one_text, box_two_text, box_three_text,
+                box_four_text, box_five_text, constraint_layout,
+                red_button, green_button, yellow_button)
 
         for (item in clickableViews){
             item.setOnClickListener { makeColored(it) }
@@ -37,6 +40,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
